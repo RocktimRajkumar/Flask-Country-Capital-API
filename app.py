84d02537cafd3d4712469ab7366c7370ed0a8249
Rocktim <rajkumar.rocktim@gmail.com>
2019-09-14 02:37:13 +0530

    app.py : flask configuration and entry point created

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ import numpy as np
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+# Configuring flask with sqlite db
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///country.db'
+db = SQLAlchemy(app)
+
+
+# entry point to run the application
+if __name__ == '__main__':
+    app.run()
