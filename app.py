b5b0e57f857c919fc0fda61028de9e21ac7a23f8
Rocktim <rajkumar.rocktim@gmail.com>
2019-09-14 02:39:50 +0530

    app.py : table or model with field created

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///country.db'
 db = SQLAlchemy(app)
 
+# table or model for sqlite db
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Country = db.Column(db.String(100), nullable=False)
+    Capital = db.Column(db.String(100), nullable=False)
 
 # entry point to run the application
 if __name__ == '__main__':
